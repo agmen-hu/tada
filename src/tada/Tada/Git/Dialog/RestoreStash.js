ad83f847d7d186384124464b9318a339ba586d3c
Hendrich Attila <hendricha@agmen.hu>
2014-02-25 17:00:47 +0100

    RestoreStash now uses the new template

--- src/tada/Tada/Git/Dialog/RestoreStash.js
+++ src/tada/Tada/Git/Dialog/RestoreStash.js
@@ -3,7 +3,7 @@ defineClass('Tada.Git.Dialog.RestoreStash', 'Tada.Git.Dialog.AbstractDialog',
     __constructor: function(options)
     {
       this.__base($.extend({
-        repositoryTemplateId: "Tada-Git-Dialog-RestoreStash"
+        repositoryTemplateId: "Tada-Git-Dialog-RepoInfo"
       }, options));
     },
 
@@ -13,16 +13,24 @@ defineClass('Tada.Git.Dialog.RestoreStash', 'Tada.Git.Dialog.AbstractDialog',
       queue
         .stash(function(err) {
           if (err) {
-            this._renderRepository(repo, { error: err });
+            this._renderRepository(repo, { message: { error: true, text: err } });
             return queue.killQueue();
           }
         }.bind(this), repo, this.arguments.drop ? "pop" : "apply")
         .refresh(function(err) {
           if (err) {
-            this._renderRepository(repo, { error: err });
+            this._renderRepository(repo, { message: { error: true, text: err } });
             return queue.killQueue();
           }
-          this._renderRepository(repo, { isDirty: this.get("git.project").getRepository(repo).getFileStatus().isDirty() });
+          this._renderRepository(repo, {
+            message: { text: "Restored stash"},
+            links: this.get("git.project").getRepository(repo).getFileStatus().isDirty() ? [{
+              sentence: "Show change summary",
+              arguments: { "for repository <value>": repo },
+              referenceText: "Show change summary",
+              autoExecute: true
+            }] : null,
+          });
         }.bind(this), repo, ["status"]);
     }
   }
